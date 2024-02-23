0a9c90a8ece32ae60e3c20a7165a9b0a9c8f95db
Mukesh Kumar <mukeskrits@gmail.com>
2024-02-23 23:24:34 +0530

    second changes

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const cors = require('cors');
 const { translate } = require('free-translate');
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res)=>{
+    try {
+        res.status(201).send({message : 'Our App is working good'});
+    } catch (error) {
+        res.status(500).send({message : error.message});
+    }
+})
 
 app.post('/api/text_translation', async(req, res)=>{
  const {text} = req.body;
